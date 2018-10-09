aa86a4b2f265fb5cf702a0aaac764b933e2d799d
Bemoty <signup.bemoty@outlook.com>
2018-10-09 21:10:25 +0200

    upload java golf

---
+++ serious/Java/java_302.java
@@ -0,0 +1,15 @@
+public class java_302 {
+
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder();
+		int i = builder.toString().length();
+		i++;
+		i++;
+		i = i * i;
+		for (int j = i / i; j <= i; ++j) {
+			builder.append(j);
+		}
+		System.out.print(builder.toString());
+	}
+
+}
